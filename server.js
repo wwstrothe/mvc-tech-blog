78f429f914ae02f56b0fcac09b7e3b13af19c821
wwstrothe <95983821+wwstrothe@users.noreply.github.com>
2022-05-15 14:20:39 -0700

    added idle timer to the site

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: process.env.DB_SESSION_SECRET,
-  cookie: {},
+  cookie: { maxAge: 10800000 },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
