b14dbb80f46e0bc5ef4d76e8c9a51013cc058bc7
wwstrothe <95983821+wwstrothe@users.noreply.github.com>
2022-05-14 18:42:55 -0700

    connected to front end and created homepage with all posts

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const apiRoutes = require("./api");
+const homeRoutes = require("./home-routes.js");
 
 router.use("/api", apiRoutes);
+router.use("/", homeRoutes);
 
 // Define a catch-all route for any resource that doesn't exist
 router.use((req, res) => {
--- server.js
+++ server.js
@@ -2,13 +2,20 @@ const path = require("path");
 require("dotenv").config();
 const express = require("express");
 const sequelize = require("./config/connection");
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({})
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.static(path.join(__dirname, "public")));
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use(require("./controllers/"));
 
